29775681a621550bb3c2bea3def403338d114b5b
najeebib <najeebibraheem1999@hotmail.com>
2024-06-23 15:45:08 +0300

    setup server and nginx config

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
 
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
@@ -7,3 +10,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello from fast api"}
+
+@app.get("/welcome-img")
+def image():
+    file_path = "images/welcome.jpg"
+    return FileResponse(file_path)
+
+@app.get("/welcome-html")
+def html(request: Request):
+    return templates.TemplateResponse("welcome.html", {"request": request})
